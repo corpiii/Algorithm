1a2c002f9621500c4e238ab22ce73821c0732bcc
jeongmin <lj845854@gmail.com>
2022-06-06 00:38:46 +0900

    로또의 최고 순위와 최저 순위

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -4,8 +4,21 @@
 //
 //  Created by 이정민 on 2022/06/03.
 //
-
 import Foundation
 
-print("Hello, World!")
+var lottos = [44, 1, 0, 0, 31, 25]
+var win_nums = [31, 10, 45, 1, 6, 19]
+print(highestLowestLotto(lottos, win_nums))
 
+lottos = [0, 0, 0, 0, 0, 0]
+win_nums = [38, 19, 20, 40, 15, 25]
+print(highestLowestLotto(lottos, win_nums))
+
+lottos = [45, 4, 35, 20, 3, 9]
+win_nums = [20, 9, 3, 45, 4, 35]
+print(highestLowestLotto(lottos, win_nums))
+
+print("//////////////////")
+lottos = [1, 2, 3, 4, 5, 6]
+win_nums = [38, 19, 20, 40, 15, 25]
+print(highestLowestLotto(lottos, win_nums))
