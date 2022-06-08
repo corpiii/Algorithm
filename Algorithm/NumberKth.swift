d88f931676d2ad14aa49bec6394115306759e75c
jeongmin <lj845854@gmail.com>
2022-06-08 13:37:25 +0900

    rename : 대문자시작으로 변경

--- Algorithm/NumberKth.swift
+++ Algorithm/NumberKth.swift
@@ -11,7 +11,10 @@ func numberKth(_ array:[Int], _ commands:[[Int]]) -> [Int] {
     var result: [Int] = []
     
     commands.forEach { command in
-        let start = command[0], end = command[1], index = command[2]
+        let start = command[0]
+        let end = command[1]
+        let index = command[2]
+        
         let newArray: [Int] = array[(start-1)...(end-1)].sorted()
         result.append(newArray[index - 1])
     }
