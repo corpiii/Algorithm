863b889b0fe1ea9c90546443955a3538da434c8d
jeongmin <lj845854@gmail.com>
2022-06-24 01:51:45 +0900

    add : 소수 찾기

---
+++ Algorithm/3주차/findPrime.swift
@@ -0,0 +1,29 @@
+//
+//  findPrime.swift
+//  Algorithm
+//
+//  Created by 이정민 on 2022/06/24.
+//
+
+import Foundation
+//
+//for number in numbers {
+//        var stack: [String] = [(number, 1)]
+//        while !stack.isEmpty {
+//            var node = stack.removeLast()
+//            print(node)
+//            pieces.append(node)
+//            if !allNumbers[node.1].contains(node.0) {
+//                allNumbers[node.1].append(node)
+//            }
+//
+//            if pieces.count == turn {
+//                allNumbers.append(pieces)
+//            }
+//            for number in numbers {
+//                if !pieces.contains(number) {
+//                    stack.append(number)
+//                }
+//            }
+//        }
+//    }
